cf019284d9e911a0666f01ea554882cd74bc9bc4
1e10 Laptop <Student@bths.edu>
2022-10-17 12:32:32 -0400

    I did something again :)

--- src/LinearEquationRunner.java
+++ src/LinearEquationRunner.java
@@ -9,8 +9,18 @@ public class LinearEquationRunner {
         int coord1Check = coord1.indexOf(",");
         int coord1Length = coord1.length() - 1;
         int x1 = Integer.parseInt(coord1.substring(1, coord1Check));
-        int x2 = Integer.parseInt(coord1.substring(coord1Check + 1, coord1Length));
+        int y1 = Integer.parseInt(coord1.substring(coord1Check + 1, coord1Length));
         System.out.println(x1);
+        System.out.println(y1);
+
+        System.out.print("Enter coordinate 2: ");
+        String coord2 = scan.next();
+        scan.nextLine();
+        int coord2Check = coord2.indexOf(",");
+        int coord2Length = coord2.length() - 1;
+        int x2 = Integer.parseInt(coord2.substring(1, coord2Check));
+        int y2 = Integer.parseInt(coord2.substring(coord2Check + 1, coord2Length));
         System.out.println(x2);
+        System.out.println(y2);
     }
 }
